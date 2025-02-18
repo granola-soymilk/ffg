c10551a8694e074082bf53600617d32bddfc6f52
granola-soymilk <bianca.bacani@mutant.be>
2025-02-18 10:51:11 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -24,43 +24,36 @@ function setup() {
         .position(20, 20);
     barsSlider = createSlider(5, 15, 10, 1)
         .position(20, 40);
-    barsSlider.input(() => BARS_X = barsSlider.value());
     
     createDiv('Quad Point:')
         .position(200, 20);
     quadSlider = createSlider(0, 40, 0, 1)
         .position(200, 40);
-    quadSlider.input(() => QUAD_POINT = quadSlider.value());
     
     createDiv('Bar Spacing:')
         .position(350, 20);
     spacingSlider = createSlider(0, 20, 2, 1)
         .position(350, 40);
-    spacingSlider.input(() => BAR_SPACING = spacingSlider.value());
     
     createDiv('Sine Amplitude:')
         .position(500, 20);
     sineSlider = createSlider(0, 1, 0, 0.1)
         .position(500, 40);
-    sineSlider.input(() => SINE_AMPLITUDE = sineSlider.value());
     
     createDiv('Height Amplitude:')
         .position(650, 20);
     heightSlider = createSlider(0, 1, 0, 0.1)
         .position(650, 40);
-    heightSlider.input(() => HEIGHT_AMPLITUDE = heightSlider.value());
     
     createDiv('Peak Position:')
         .position(800, 20);
     peakSlider = createSlider(0, 1, 0.5, 0.1)
         .position(800, 40);
-    peakSlider.input(() => PEAK_POSITION = peakSlider.value());
     
     createDiv('X Offset:')
         .position(950, 20);
     xOffsetSlider = createSlider(-100, 50, 0, 1)
         .position(950, 40);
-    xOffsetSlider.input(() => X_OFFSET = xOffsetSlider.value());
 }
 
 function draw() {
